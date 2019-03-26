8ddcfb0f1b01a89aafec446a27d85e9f768d577a
Noiri <suleipenn@gmail.com>
2019-03-27 02:36:49 +0900

    implements eventsShow componet

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,14 +1,26 @@
 import _ from 'lodash';
-import {READ_EVENTS, DELETE_EVENT} from '../actions';
+import {
+    CREATE_EVENT,
+    READ_EVENTS,
+    DELETE_EVENT,
+    READ_EVENT,
+    UPDATE_EVENT
+} from '../actions';
 
 export default (events = {}, action) => {
     switch (action.type) {
         case READ_EVENTS:
+        case CREATE_EVENT:
             return _.mapKeys(action.response.data, 'id');
         case DELETE_EVENT:
             //eventsというオブジェクトの要素を削除する
             delete events[action.id];
             return {...events};
+        case READ_EVENT:
+            const data = action.response.data;
+            return {...events, [data.id]: data};
+        case UPDATE_EVENT:
+            return _.mapKeys(action.response.data, 'id');
         default:
             return events;
     }
